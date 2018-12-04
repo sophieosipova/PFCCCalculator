0a25d61b136b1a4be11414eb49ffeddad7d96e83
sophie <osipovaa123@yandex.ru>
2018-12-04 16:44:19 +0300

    Method for comparing comments were added

--- Comments/Models/Comments.cs
+++ Comments/Models/Comments.cs
@@ -7,12 +7,25 @@ using System.Threading.Tasks;
 namespace CommentsService.Models
 {
     [Table(Name = "tblComments")]
-    public class Comment
+    public class Comment : IEquatable<Comment>
     {
         [Column(IsPrimaryKey = true, IsDbGenerated = true)]
         public int CommentId { get; set; }
         public string CommentText{ get; set; }
         public int UserId { get; set; }
         public int DishId { get; set; }
+
+        public bool Equals(Comment other)
+        {
+            return other != null &&
+                CommentId == other.CommentId &&
+                CommentText == other.CommentText &&
+                UserId == other.UserId &&
+                DishId == other.DishId;
+        }
+        public override int GetHashCode()
+        {
+            return this.CommentId.GetHashCode();
+        }
     }
 }
