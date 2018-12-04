7827da13bd4a1a71c22a1d91eae51e370ca27f09
sophie <osipovaa123@yandex.ru>
2018-12-04 17:17:27 +0300

    Some tests for comments service were added

--- Tests/CommentsServiceTests/GetCommentsTests.cs
+++ Tests/CommentsServiceTests/GetCommentsTests.cs
@@ -84,14 +84,89 @@ namespace Tests.CommentsServiceTests
             Assert.IsType<BadRequestResult>(result);
         }
 
-        private List<Comment> GetTestComments()
+
+        [Fact]
+        public async void GetCommentsByDishIdTest()
+        {
+            // Arrange
+            int dishTestID = 1;
+            int pageSize = 1;
+            int pageIndex = 0;
+            var mockRepo = new Mock<ICommentsRepository>();
+            mockRepo.Setup(c => c.GetCommentsByDishId(dishTestID,pageSize,pageIndex))
+                .ReturnsAsync(GetTestPaginatedComments(dishTestID, pageSize, pageIndex));
+            var controller = new CommentsService.Controllers.CommentsController(mockRepo.Object);
+
+
+            // Act
+            var result = await controller.GetCommentsByDishId(dishTestID, pageSize, pageIndex);
+
+
+            // Assert
+            var requestResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsType<PaginatedModel<Comment>>(requestResult.Value);
+            var testmodel = GetTestPaginatedComments(dishTestID, pageSize, pageIndex);
+            Assert.Equal(model.Count, testmodel.Count);
+            Assert.Equal(model.Data, testmodel.Data);
+            Assert.Equal(model.PageIndex, testmodel.PageIndex);
+            Assert.Equal(model.PageSize, testmodel.PageSize);
+
+        }
+
+        [Fact]
+        public async void GetNullCommentByDishIdTest()
+        {
+            // Arrange
+            int dishTestID = 1;
+            int pageSize = 1;
+            int pageIndex = 0;
+            var mockRepo = new Mock<ICommentsRepository>();
+            mockRepo.Setup(c => c.GetCommentsByDishId(dishTestID, pageSize, pageIndex))
+                .ReturnsAsync(GetNullPaginatedCommentTest(dishTestID, pageSize, pageIndex));
+            var controller = new CommentsService.Controllers.CommentsController(mockRepo.Object);
+
+
+            // Act
+            var result = await controller.GetCommentsByDishId(dishTestID, pageSize, pageIndex);
+
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+
+        [Fact]
+        public async void GetBadRequestCommentsByUserIdTest()
+        {
+            // Arrange
+            int dishTestID = -1;
+            int pageSize = 1;
+            int pageIndex = 0;
+            var mockRepo = new Mock<ICommentsRepository>();
+            mockRepo.Setup(c => c.GetCommentsByDishId(dishTestID, pageSize, pageIndex))
+                .ReturnsAsync(GetNullPaginatedCommentTest(dishTestID, pageSize, pageIndex));
+            var controller = new CommentsService.Controllers.CommentsController(mockRepo.Object);
+
+
+            // Act
+            var result = await controller.GetCommentsByDishId(dishTestID, pageSize, pageIndex);
+
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        private PaginatedModel<Comment>  GetTestPaginatedComments(int dishTestID, int pageSize, int pageIndex)
         {
             List<Comment> comments = new List<Comment>();
 
             comments.Add( new Comment { UserId = 1, CommentText = "Тест 1", DishId = 1 });
             comments.Add(new Comment { UserId = 2, CommentText = "Тест 2", DishId = 2 });
 
-            return comments;
+
+            PaginatedModel<Comment> paginatedModel = new PaginatedModel<Comment>
+                (0, 1, 2, comments);
+            return paginatedModel;
         }
 
 
@@ -103,6 +178,11 @@ namespace Tests.CommentsServiceTests
         }
 
         private Comment GetNullCommentTest(int commentID)
+        {
+            return null;
+        }
+
+        private PaginatedModel<Comment> GetNullPaginatedCommentTest(int dishTestID, int pageSize, int pageIndex)
         {
             return null;
         }
